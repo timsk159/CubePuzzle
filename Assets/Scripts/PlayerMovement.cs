1ead6e7e542368111d53872433f8f3579c45b8f5
timsk159 <timsk951@hotmail.co.uk>
2013-12-13 08:10:10 +0000

    Restructured PlayerMovement.cs to make it easier to add movement types for testing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using System;
 using System.Collections;
+
+public enum MovementType
+{
+	Physics, Block, Smooth
+}
 
 public class PlayerMovement : MonoBehaviour 
 {
-	private bool physicsMovement = true;
+	MovementType[] allMoveTypes;
+
+	MovementType moveType;
 
 	public bool canMove = true;
 	public bool isMoving;
@@ -30,27 +38,39 @@ public class PlayerMovement : MonoBehaviour
 	float movementCD = 0.5f;
 	float nextMoveTime;
 
+	void Start()
+	{
+		allMoveTypes = (MovementType[])Enum.GetValues(typeof(MovementType));
+	}
+
 	void Update()
 	{
 		if(Input.GetKeyDown(KeyCode.F1))
 		{
-			physicsMovement = !physicsMovement;
-			if(!physicsMovement)
+			CycleMoveType();
+			switch(moveType)
 			{
-				var roundedPos = transform.position;
-				roundedPos.x = Mathf.RoundToInt(roundedPos.x);
-				roundedPos.z = Mathf.RoundToInt(roundedPos.z);
-				transform.rotation = Quaternion.identity;
-				transform.position = roundedPos;
+				case MovementType.Physics:
+					rigidbody.freezeRotation = false;
+					rigidbody.useGravity = true;
+					rigidbody.isKinematic = false;
+					break;
+				case MovementType.Block:
+					var roundedPos = transform.position;
+					roundedPos.x = Mathf.RoundToInt(roundedPos.x);
+					roundedPos.z = Mathf.RoundToInt(roundedPos.z);
+					transform.rotation = Quaternion.identity;
+					transform.position = roundedPos;
 
-				rigidbody.velocity = Vector3.zero;
-				rigidbody.useGravity = false;
-				rigidbody.freezeRotation = true;
-			}
-			else
-			{
-				rigidbody.freezeRotation = false;
-				rigidbody.useGravity = true;
+					rigidbody.velocity = Vector3.zero;
+					rigidbody.useGravity = false;
+					rigidbody.freezeRotation = true;
+					break;
+				case MovementType.Smooth:
+					rigidbody.freezeRotation = true;
+					rigidbody.useGravity = false;
+					rigidbody.isKinematic = true;
+					break;
 			}
 			canMove = true;
 		}
@@ -70,32 +90,17 @@ public class PlayerMovement : MonoBehaviour
 				var xForce = horizontalInput * inputSensitivity;
 
 				var force = new Vector3(xForce, 0, zForce);
-				if(physicsMovement)
+				switch(moveType)
 				{
-					if(rigidbody.velocity.magnitude < maxSpeed)
-						rigidbody.AddForce(force, ForceMode.Acceleration);
-				}
-				else
-				{
-					if(Time.time > nextMoveTime)
-					{
-						//Normalize doesn't work for negative-length vectors. Have to normalize it manually.
-						//force.Normalize();
-
-						force.x = Mathf.Clamp(force.x, -1, 1);
-						force.z = Mathf.Clamp(force.z, -1, 1);
-
-						force.x = Mathf.CeilToInt(force.x);
-						force.z = Mathf.CeilToInt(force.z);
-
-						var movementDistance = Mathf.Abs(force.x) + Mathf.Abs(force.z);
-						if(!Physics.Raycast(new Ray(transform.position, force), movementDistance))
-						{
-							canMove = false;
-							iTween.MoveAdd(gameObject, iTween.Hash("amount", force, "time", 0.5f, "easetype", iTween.EaseType.linear, "oncomplete", "MoveComplete"));
-							nextMoveTime = Time.time + movementCD;
-						}
-					}
+					case MovementType.Physics:
+						DoPhysicsMovement(force);
+						break;
+					case MovementType.Block:
+						DoBlockMovement(force);
+						break;
+					case MovementType.Smooth:
+						DoSmoothMovement(force);
+						break;
 				}
 			}
 			else
@@ -103,6 +108,52 @@ public class PlayerMovement : MonoBehaviour
 				isMoving = false;
 			}
 		}
+	}
+
+	void CycleMoveType()
+	{
+		var currentIndex = (int)moveType;
+
+		currentIndex++;
+
+		if(currentIndex >= allMoveTypes.Length)
+			currentIndex = 0;
+
+		moveType = (MovementType)currentIndex;
+	}
+
+	void DoPhysicsMovement(Vector3 input)
+	{
+		if(rigidbody.velocity.magnitude < maxSpeed)
+			rigidbody.AddForce(input, ForceMode.Acceleration);
+	}
+
+	void DoBlockMovement(Vector3 input)
+	{
+		if(Time.time > nextMoveTime)
+		{
+			//Normalize doesn't work for negative-length vectors. Have to normalize it manually.
+			//force.Normalize();
+
+			input.x = Mathf.Clamp(input.x, -1, 1);
+			input.z = Mathf.Clamp(input.z, -1, 1);
+
+			input.x = Mathf.CeilToInt(input.x);
+			input.z = Mathf.CeilToInt(input.z);
+
+			var movementDistance = Mathf.Abs(input.x) + Mathf.Abs(input.z);
+			if(!Physics.Raycast(new Ray(transform.position, input), movementDistance))
+			{
+				canMove = false;
+				iTween.MoveAdd(gameObject, iTween.Hash("amount", input, "time", 0.5f, "easetype", iTween.EaseType.linear, "oncomplete", "MoveComplete"));
+				nextMoveTime = Time.time + movementCD;
+			}
+		}
+	}
+
+	void DoSmoothMovement(Vector3 input)
+	{
+
 	}
 
 	void MoveComplete()
