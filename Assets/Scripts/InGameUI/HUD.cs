c53846954b19a42828f16c0f1a119608f0c2a50e
timsk159 <timsk951@hotmail.co.uk>
2014-01-02 08:57:28 +0000

    Coded and added placeholder art for the colour cycler on the HuD

--- Assets/Scripts/InGameUI/HUD.cs
+++ Assets/Scripts/InGameUI/HUD.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class HUD : MonoBehaviour 
 {
-	UITexture colourCycleImage;
-	UITexture colourCycleHighlight;
+	GameObject rootPanel;
+	GameObject colourCycleHighlight;
+
+	Vector3 redHighlightPos;
+	Vector3 greenHighlightPos;
+	Vector3 blueHighlightPos;
+
 
 	void Start()
 	{
 		Messenger<Colour>.AddListener(ColourCollisionMessage.PlayerChangedColour.ToString(), PlayerChangedColour);
+
+		rootPanel = GameObject.Find("HudPanel");
+		colourCycleHighlight = rootPanel.transform.Find("ColourCycleRoot/ColourHighlight").gameObject;
+
+		redHighlightPos = new Vector3(-0.33333f, 0, 0);
+		greenHighlightPos = Vector3.zero;
+		blueHighlightPos = new Vector3(0.33333f, 0, 0);
 	}
 
 	void OnDestroy()
@@ -21,17 +33,29 @@ public class HUD : MonoBehaviour
 		switch(colourToChangeTo)
 		{
 			case Colour.Blue:
-				//colourCycleHighlight.transform.position = ;
+				colourCycleHighlight.transform.localPosition = blueHighlightPos;
 				break;
 			case Colour.Green:
-
+				colourCycleHighlight.transform.localPosition = greenHighlightPos;
 				break;
 			case Colour.Red:
-
+				colourCycleHighlight.transform.localPosition = redHighlightPos;
 				break;
 			default:
 				Debug.LogError("Unknown colour");
 				break;
 		}
+	}
+
+
+
+	void EnableHud()
+	{
+		rootPanel.SetActive(true);
+	}
+
+	void DisableHud()
+	{
+		rootPanel.SetActive(false);
 	}
 }
