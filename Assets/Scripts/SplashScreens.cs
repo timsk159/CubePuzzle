6e4a461025dde783971a7391c1a162a7367f937a
timsk159 <timsk951@hotmail.co.uk>
2014-02-12 18:20:50 +0000

    Added security checks for webplayer

--- Assets/Scripts/SplashScreens.cs
+++ Assets/Scripts/SplashScreens.cs
@@ -1,15 +1,130 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-public class SplashScreens : MonoBehaviour {
+public class SplashScreens : MonoBehaviour 
+{
+	public List<SplashScreen> splashScreens;
+	public SplashScreen currentSplash;
 
-	// Use this for initialization
-	void Start () {
-	
+	public AudioSource audioS;
+	public UITexture uiTexture;
+
+	public bool canSkip;
+
+	public void Start()
+	{
+#if DEMO_VERSION
+		var webSecure = new WebSecure();
+
+		if(!webSecure.IsHostedCorrectly())
+		{
+			//Make sure to modify url for each site thats hosting below (seperate build for each site)
+			FailedSecurityUI();
+			return;
+		}
+#endif
+		StartCoroutine(SplashScreenDisplayRoutine());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void FailedSecurityUI()
+	{
+
+	}
+
+	IEnumerator SplashScreenDisplayRoutine(int indexToStart = 0)
+	{
+		if(indexToStart > splashScreens.Count)
+		{
+			throw new System.ArgumentException("Start index exceeeded list count", "indexToStart");
+		}
+
+		for(int i = indexToStart; i < splashScreens.Count; i++)
+		{
+			canSkip = false;
+
+			currentSplash = splashScreens[i];
+			currentSplash.Show(uiTexture, audioS);
+
+			if(currentSplash.timeTillSkippable == 0)
+			{
+				canSkip = true;
+			}
+			else
+			{
+				yield return new WaitForSeconds(currentSplash.timeTillSkippable);
+				canSkip = true;
+			}
+
+			yield return new WaitForSeconds(currentSplash.timeToShow);
+		}
+		SplashScreensFinished();
+	}
+
+	void SkipSplash()
+	{
+		StopAllCoroutines();
+		canSkip = false;
+
+		var index = splashScreens.IndexOf(currentSplash);
+		index++;
+
+		if(index > splashScreens.Count)
+		{
+			SplashScreensFinished();
+		}
+		else
+		{
+			StartCoroutine(SplashScreenDisplayRoutine(index));
+		}
+	}
+
+	void Update()
+	{
+		if(canSkip)
+		{
+			if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Return))
+			{
+				SkipSplash();
+			}
+		}
+	}
+
+	void SplashScreensFinished()
+	{
+		if(Application.CanStreamedLevelBeLoaded("FrontMenu"))
+		{
+			Application.LoadLevel("FrontMenu");
+		}
+	}
+}
+
+
+public class SplashScreen
+{
+	public Texture2D splashImage;
+	public AudioClip audio;
+	public float timeToShow;
+	public float timeTillSkippable;
+
+	public void Show(UITexture uiTexture, AudioSource audioS)
+	{
+		uiTexture.mainTexture = splashImage;
+		if(audioS != null)
+		{
+			audioS.clip = audio;
+			audioS.Play();
+		}
+	}
+
+	public void Show(Material mat, AudioSource audioS)
+	{
+		mat.mainTexture = splashImage;
+		if(audioS != null)
+		{
+			audioS.clip = audio;
+			audioS.Play();
+		}
 	}
 }
---
+++ Assets/Scripts/WebSecure.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebSecure
+{
+	public bool IsHostedCorrectly(string fullCorrectURL)
+	{
+		if(Application.isEditor)
+		{
+			return true;
+		}
+
+		if(Application.genuineCheckAvailable)
+		{
+			if(!Application.genuine)
+			{
+				return false;
+			}
+		}
+
+		if(Application.isWebPlayer)
+		{
+			if(Application.srcValue != "Cubaze.unity3d")
+				return false;
+			if(string.Compare(Application.absoluteURL, fullCorrectURL) != 0)
+				return false;
+		}
+
+		return true;
+	}
+}
