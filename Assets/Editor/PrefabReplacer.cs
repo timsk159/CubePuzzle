4cd6d0c1f2548c80e068a6dbebc7237c4bb13789
timsk159 <timsk951@hotmail.co.uk>
2014-02-14 09:38:25 +0000

    Added prefabreplace tool to help with setting up story levels. Replaced a load of stuff in story levels with prefabs

---
+++ Assets/Editor/PrefabReplacer.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PrefabReplacer : EditorWindow 
+{
+	List<string> namesOfGameobjectsToReplace;
+	GameObject prefab;
+	bool matchScale;
+
+	int indexToReplace;
+
+	[MenuItem("Window/Prefab Replacer")]
+	static void Init()
+	{
+		var window = GetWindow<PrefabReplacer>("Prefab Replacer");
+		window.namesOfGameobjectsToReplace = new List<string>();
+		window.indexToReplace = -1;
+	}
+
+	void OnGUI()
+	{
+		prefab = (GameObject)EditorGUILayout.ObjectField(prefab, typeof(GameObject), false);
+
+		matchScale = EditorGUILayout.Toggle("Match Scale?", matchScale);
+
+		EditorGUILayout.Space();
+
+		GUILayout.Label("Names: ");
+		if(GUILayout.Button("Add Name"))
+		{
+			namesOfGameobjectsToReplace.Add("");
+		}
+		for(int i = 0; i < namesOfGameobjectsToReplace.Count; i++)
+		{
+			GUILayout.BeginHorizontal();
+			namesOfGameobjectsToReplace[i] = GUILayout.TextField(namesOfGameobjectsToReplace[i]);
+
+			if(GUILayout.Button("-"))
+			{
+				indexToReplace = i;
+			}
+			GUILayout.EndHorizontal();
+		}
+
+		if(indexToReplace != -1)
+		{
+			namesOfGameobjectsToReplace.RemoveAt(indexToReplace);
+			indexToReplace = -1;
+		}
+
+		if(GUILayout.Button("Replace!"))
+		{
+			if(prefab != null && namesOfGameobjectsToReplace.Count != 0)
+				ReplaceGameobjects();
+		}
+	}
+
+	void ReplaceGameobjects()
+	{
+		var gameobjectsToReplace = new List<GameObject>();
+
+		var allGameobjects = GameObject.FindObjectsOfType<GameObject>();
+
+		gameobjectsToReplace = allGameobjects.ToList().Where(go => namesOfGameobjectsToReplace.Contains(go.name)).ToList();
+
+		foreach(var go in gameobjectsToReplace)
+		{
+			var newGO = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+
+			MatchTransform(go, newGO);
+
+			DestroyImmediate(go);
+		}
+	}
+
+	void MatchTransform(GameObject oldGO, GameObject newGO)
+	{
+		newGO.transform.parent = oldGO.transform.parent;
+
+		var pos = oldGO.transform.localPosition;
+		var rot = oldGO.transform.localRotation;
+		var scal = oldGO.transform.localScale;
+
+		newGO.transform.localPosition = pos;
+		newGO.transform.localRotation = rot;
+		if(matchScale)
+			newGO.transform.localScale = scal;
+	}
+}
